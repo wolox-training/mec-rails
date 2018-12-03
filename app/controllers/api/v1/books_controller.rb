93d403f0c1567af99847dfd7db56e4f5c4f15f91
mcolombo87 <mcolombo87@gmail.com>
2018-12-03 18:11:20 -0300

    Authentication is requiered for api request

--- app/controllers/api/v1/books_controller.rb
+++ app/controllers/api/v1/books_controller.rb
@@ -2,6 +2,7 @@ module Api
   module V1
     class BooksController < ApplicationController
       include Wor::Paginate
+      before_action :authenticate_user!
       # GET /books
       def index
         render_paginated Book
