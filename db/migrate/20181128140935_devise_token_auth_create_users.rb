3a1b907ff577a20554b5b5987a9b3fb08612ae39
mcolombo87 <mcolombo87@gmail.com>
2018-12-04 16:39:08 -0300

    Resolved conflict and change migration file

--- db/migrate/20181128140935_devise_token_auth_create_users.rb
+++ db/migrate/20181128140935_devise_token_auth_create_users.rb
@@ -3,32 +3,19 @@ class DeviseTokenAuthCreateUsers < ActiveRecord::Migration[5.2]
     table = :users
     change_table(table) do |t|
       ## Required
-      t.string :provider, :null => false, :default => "email" unless column_exists?(table, :provider)
-      t.string :uid, :null => false, :default => "" unless column_exists?(table, :uid)
+      t.string :provider, null: false, default: "email" unless column_exists?(table, :provider)
+      t.string :uid, null: false, default: "" unless column_exists?(table, :uid)
 
       ## Database authenticatable
-      t.string :encrypted_password, :null => false, :default => "" unless column_exists?(table, :encrypted_password)
+      t.string :encrypted_password, null: false, default: "" unless column_exists?(table, :encrypted_password)
 
       ## Recoverable
       t.string   :reset_password_token unless column_exists?(table, :reset_password_token)
       t.datetime :reset_password_sent_at unless column_exists?(table, :reset_password_sent_at)
-      t.boolean  :allow_password_change, :default => false unless column_exists?(table, :allow_password_change)
+      t.boolean  :allow_password_change, default: false unless column_exists?(table, :allow_password_change)
 
       ## Rememberable
       t.datetime :remember_created_at unless column_exists?(table, :created_at)
-
-      ## Trackable
-      t.integer  :sign_in_count, :default => 0, :null => false unless column_exists?(table, :sign_in_count)
-      t.datetime :current_sign_in_at unless column_exists?(table, :current_sign_in_at)
-      t.datetime :last_sign_in_at unless column_exists?(table, :last_sign_in_at)
-      t.string   :current_sign_in_ip unless column_exists?(table, :current_sign_in_ip)
-      t.string   :last_sign_in_ip unless column_exists?(table, :last_sign_in_ip)
-
-      ## Confirmable
-      t.string   :confirmation_token unless column_exists?(table, :confirmation_token)
-      t.datetime :confirmed_at unless column_exists?(table, :confirmed_at)
-      t.datetime :confirmation_sent_at unless column_exists?(table, :confirmation_sent_at)
-      t.string   :unconfirmed_email unless column_exists?(table, :unconfirmed_email) # Only if using reconfirmable
 
       ## Lockable
       # t.integer  :failed_attempts, :default => 0, :null => false # Only if lock strategy is :failed_attempts
