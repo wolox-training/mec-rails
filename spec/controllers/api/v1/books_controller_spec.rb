e1daa6370e4199eeee8e58883cf3aa09c8e99365
mcolombo87 <mcolombo87@gmail.com>
2018-12-06 16:13:39 -0300

    Add response helper in rspec and some fixes requiered in review

--- spec/controllers/api/v1/books_controller_spec.rb
+++ spec/controllers/api/v1/books_controller_spec.rb
@@ -1,34 +1,23 @@
 require 'rails_helper'
-require 'faker'
-
-shared_context 'Authenticated User' do
-  include Devise::Test::ControllerHelpers
-  let(:user) { create(:user) }
-  before do
-    request.headers.merge! user.create_new_auth_token
-  end
-end
+require 'support/shared_contexts/shared_authenticated.rb'
 
 describe Api::V1::BooksController, type: :controller do
   include_context 'Authenticated User'
 
-  before(:all) do
-    @book = create(:book)
-    @rent = Rent.create(
-      user_id: 1,
-      book_id: 1,
-      start: Time.zone.now,
-      end: Time.zone.now
-    )
-  end
   describe 'GET #index' do
     context 'When fetching all the users rents' do
       before do
-        request.headers.merge! user.create_new_auth_token
+        @book = create(:book)
+        @rent = Rent.create(
+          user_id: 1,
+          book_id: 1,
+          start: Time.zone.now,
+          end: Time.zone.now
+        )
         get :index
       end
       it 'Json with all rents for this user' do
-        expect(JSON.parse(response.body.as_json)['page'][0]['id']).to eq(@book.as_json['id'])
+        expect(response_body['page'][0]['id']).to eq(@book.as_json['id'])
       end
       it 'responds with 200 status' do
         expect(response).to have_http_status(:ok)
@@ -39,12 +28,12 @@ describe Api::V1::BooksController, type: :controller do
   describe 'GET #show' do
     context 'When show a specific book' do
       before do
-        request.headers.merge! user.create_new_auth_token
-        get :show, params: { id: 1 }
+        @book = create(:book)
+        get :show, params: { id: @book.id }
       end
 
       it 'responses with the book as json' do
-        expect(response.body.as_json).to eq(@book.to_json)
+        expect(response_body.to_json).to eq(@book.to_json)
       end
       it 'responds with 200 status' do
         expect(response).to have_http_status(:ok)
