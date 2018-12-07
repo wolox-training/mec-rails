20d1221fabb1ef38b61ad8eca1d01b23d7b566ca
mcolombo87 <mcolombo87@gmail.com>
2018-12-07 13:00:41 -0300

    Changes in Book Controller Tests, remove new variables from 'before' and put them into let declaration

--- spec/controllers/api/v1/books_controller_spec.rb
+++ spec/controllers/api/v1/books_controller_spec.rb
@@ -1,23 +1,16 @@
 require 'rails_helper'
-require 'support/shared_contexts/shared_authenticated.rb'
 
 describe Api::V1::BooksController, type: :controller do
   include_context 'Authenticated User'
 
   describe 'GET #index' do
     context 'When fetching all the users rents' do
+      let!(:book) { create(:book) }
       before do
-        @book = create(:book)
-        @rent = Rent.create(
-          user_id: 1,
-          book_id: 1,
-          start: Time.zone.now,
-          end: Time.zone.now
-        )
         get :index
       end
       it 'Json with all rents for this user' do
-        expect(response_body['page'][0]['id']).to eq(@book.as_json['id'])
+        expect(response_body['page'][0]['id']).to eq(book.id)
       end
       it 'responds with 200 status' do
         expect(response).to have_http_status(:ok)
@@ -27,15 +20,15 @@ describe Api::V1::BooksController, type: :controller do
 
   describe 'GET #show' do
     context 'When show a specific book' do
+      let!(:book) { create(:book) }
       before do
-        @book = create(:book)
-        get :show, params: { id: @book.id }
+        get :show, params: { id: book.id }
       end
 
       it 'responses with the book as json' do
         expect(
           response_body['id']
-        ).to eq(@book.id)
+        ).to eq(book.id)
       end
       it 'responds with 200 status' do
         expect(response).to have_http_status(:ok)
