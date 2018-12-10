eedd7430965d032c4d2fd91494476a2c3503fd78
mcolombo87 <mcolombo87@gmail.com>
2018-12-10 12:29:51 -0300

    'rent_params' call a missed method. Fix it.

--- app/controllers/api/v1/rents_controller.rb
+++ app/controllers/api/v1/rents_controller.rb
@@ -9,12 +9,16 @@ module Api
 
       def create
         user = User.find(params[:user_id])
-        new_rent = user.rents.new(create_rent_params)
+        new_rent = user.rents.new(rent_params)
         if new_rent.save
           render json: new_rent, serializer: RentSerializer, status: :created
         else
           render json: { errors: new_rent.errors }, status: :unprocessable_entity
         end
+      end
+
+      def rent_params
+        params.require(:rent).permit(:user_id, :book_id, :start, :end)
       end
     end
   end
