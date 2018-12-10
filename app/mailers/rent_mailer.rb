ea2911171eef3e41abd342421bdcbea252681319
mcolombo87 <mcolombo87@gmail.com>
2018-12-10 12:09:47 -0300

    New translations for each user rent

--- app/mailers/rent_mailer.rb
+++ app/mailers/rent_mailer.rb
@@ -3,6 +3,6 @@ class RentMailer < ApplicationMailer
     @user = rent.user
     @rent = rent
     @book = rent.book
-    mail(to: @user.email, subject: 'New Rent was Created')
+    mail(to: @user.email, subject: I18n.t('email.new_rent.mail_header'))
   end
 end
