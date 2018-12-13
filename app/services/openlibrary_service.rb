ae2562bf0191489bd863a872eb351374f576e964
mcolombo87 <mcolombo87@gmail.com>
2018-12-13 17:52:27 -0300

    Refactoring OpenLibraryService for better

--- app/services/openlibrary_service.rb
+++ app/services/openlibrary_service.rb
@@ -1,17 +1,24 @@
 class OpenLibraryService
   include HTTParty
   base_uri 'openlibrary.org'
-  def initialize
-    @options = { query: { format: 'json', jscmd: 'data' } }
+  def book_info(isbn)
+    @isbn = isbn
+    response = self.class.get('/api/books', request_body)
+    parsing_isbn_response(response)
   end
 
-  def book_info(isbn)
-    ibsn_formatted = 'ISBN:'.concat(isbn)
-    @options[:query][:bibkeys] = ibsn_formatted
-    response = self.class.get('/api/books', @options)
+  def request_body
+    { query: { format: 'json', jscmd: 'data', bibkeys: ibsn_formatted } }
+  end
+
+  def parsing_isbn_response(response)
     json_data = JSON.parse response.body
     json_data = json_data[ibsn_formatted]
     { ibsn: isbn, title: json_data['title'], subtitle: json_data['subtitle'],
       number_of_pages: json_data['number_of_pages'], authors: json_data['authors'] }
+  end
+
+  def ibsn_formatted
+    'ISBN:'.concat(@isbn)
   end
 end
