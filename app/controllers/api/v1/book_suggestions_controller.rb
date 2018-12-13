fc6ee6e033b05de10897805b53ac7a9ac3618161
mcolombo87 <mcolombo87@gmail.com>
2018-12-13 16:09:31 -0300

    Now an user authenticated is mandatory and not optional

--- app/controllers/api/v1/book_suggestions_controller.rb
+++ app/controllers/api/v1/book_suggestions_controller.rb
@@ -1,8 +1,9 @@
 module Api
   module V1
     class BookSuggestionsController < ApplicationController
+      before_action :authenticate_user!
       def create
-        params[:book_suggestion][:user_id] = current_user.id if user_signed_in?
+        params[:book_suggestion][:user_id] = current_user.id
         new_book_sugges = BookSuggestion.new(book_sugges_params)
         if new_book_sugges.save
           render json: new_book_sugges, serializer: BookSuggestionSerializer, status: :created
